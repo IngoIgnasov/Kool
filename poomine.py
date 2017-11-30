4566724cc914c0817c48f4ed5f3da900b6d065e7
Ingo Ignašov <ingo.ignasov@gmail.com>
2017-11-30 13:49:39 +0200

    kammajaa

--- poomine.py
+++ poomine.py
@@ -23,12 +23,15 @@ root.geometry(ekraani_suurus(700, 700))
 frame = tk.Frame(root)
 frame.pack(fill=tk.BOTH, expand=1)
 
+logo = tk.PhotoImage(file="noose.gif")
+w1 = tk.Label(frame, image=logo).pack(side="right")
+
+
 msg = tk.Message(frame,text = "POOMISMÄNG")
 msg.config(bg = "lightgreen", font = ("times", 60),aspect = 500,pady=20,width = 1000)
 msg.pack(pady=10)
 nupp1=tk.Button(frame,text = "Start(avan uue akna)", command=lambda: ekraani_vahetamine())
 nupp1.pack(anchor=tk.SW)
-
 
 root.mainloop()
     
