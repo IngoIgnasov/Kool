fc0ab13dabfccd2bf78dbc70c612b65c5080a05b
Leemet <33695127+markusleemet@users.noreply.github.com>
2017-11-30 21:27:56 +0200

    tegein enda jargmise lehe menuud veits edasi

--- poomine.py
+++ poomine.py
@@ -8,12 +8,22 @@ def ekraani_suurus(w, h):
     y = (sh - h) / 2
     return ('%dx%d+%d+%d' % (w, h, x, y))
 
-def ekraani_vahetamine(*milline):
-    uus_frame = tk.Frame(root)
-    uus_frame.config(bg="green")
-    uus_frame.tkraise()
+def raskusastme_valimine(*milline):
+    global Frame
+
     frame.pack_forget()
-    uus_frame.pack(fill=tk.BOTH, expand=1)
+
+    menu2_frame = tk.Frame(root, width=root.winfo_width(), height=350, bg="green")
+    menu2_frame.config()
+    menu2_frame.tkraise()
+    menu2_frame.pack(fill="both", expand=1)
+
+    pealkiri = tk.Label(menu2_frame, text= "Keel ja raskusaste", font = ("Times", 40))
+
+    pealkiri.config(bg="yellow")
+    pealkiri.pack(fill="x")
+
+
 
 root = tk.Tk()
 
@@ -26,7 +36,7 @@ logo = tk.PhotoImage(file="noose.gif")
 
 msg = tk.Message(frame,text = "POOMISMÄNG")
 msg.config(bg = "lightgreen", font = ("times", 60),aspect = 500,pady=20,width = 1000)
-nupp1=tk.Button(frame,text = "Start(avan uue akna)",font = ("arial",30), command=ekraani_vahetamine)
+nupp1=tk.Button(frame,text = "Start(avan uue akna)",font = ("arial",30), command=raskusastme_valimine)
 nupp2= tk.Button(frame,text = "Välju",font = ("arial",30), command = root.destroy)
 msg.pack(pady=10)
 w1 = tk.Label(frame, image=logo).pack()
