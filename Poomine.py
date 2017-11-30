2d487bed7fbcf3df16578e29d4aeddda1cb26895
Ingo Ignašov <ingo.ignasov@gmail.com>
2017-11-30 12:25:45 +0200

    Uus fail

--- Poomine.py
+++ Poomine.py
@@ -1,6 +1,17 @@
-'''
-Created on 25. nov 2017
 
-@author: Ingo
-'''
-print("Tere")
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Poomismäng")
+root.geometry("1000x700")
+frame = tk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=1)
+
+msg = tk.Message(frame,text = "POOMISMÄNG")
+msg.config(bg = "lightgreen", font = ("times", 60),aspect = 500,pady=20,width = 1000)
+msg.pack(pady=10)
+nupp1=tk.Button(frame,text = "Start")
+nupp1.pack(anchor=tk.SW)
+root.mainloop()
+    
