f7c5ac77cf7a45dcce76fcc2aa15530801713698
Ingo Ignašov <ingo.ignasov@gmail.com>
2017-12-17 21:54:20 +0200

    Lisasin mõnued failid, tegin andmete lugemise proj.

--- poomine.py
+++ poomine.py
@@ -1,7 +1,13 @@
 import tkinter as tk
-
+from testproj import teesõnadjatähed
 from tkinter import PhotoImage
 
+m = teesõnadjatähed("eesti_keel.txt")
+sõnad = m[0]
+tähed = m[1]
+print(sõnad[500])
+print(tähed)
+print(len(tähed))
 def ekraani_suurus(w, h):
     sw = root.winfo_screenwidth()
     sh = root.winfo_screenheight()
@@ -18,23 +24,35 @@ def raskusastme_valimine():
     menu2_frame.tkraise()
     menu2_frame.pack(fill="both", expand=1)
 
-    """
-    joonistamine1 = tk.Canvas(menu2_frame, width=200, height=200)
-    joonistamine1.pack()
-    joonistamine1.create_line(0, 0, 200, 100)
-    joonistamine1.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
-    """
-
     pealkiri = tk.Label(menu2_frame, text= "Keel ja raskusaste", font = ("Times", 40))
 
     pealkiri.config(bg="yellow")
     pealkiri.pack(fill="x")
     
+def põhiaken():
+    #loon 3 eraldi frame, kuhu panna sõna, nupud ja poomispilt
+    global frame
+    frame.pack_forget()
+    taustraam = tk.Frame(root)
+    taustraam.config(bg="green")
+    taustraam.pack(fill="both",expand =1)
+    
+    tekstiraam=tk.Frame(root,width = 1000,height=150)
+    tekstiraam.config(bg="red")
+    tekstiraam.pack()
+    
+    nupuraam = tk.Frame(root,width = 700,height=550)
+    nupuraam.config(bg="blue")
+    nupuraam.pack(side="left")
+    
+    pildiraam = tk.Frame(root,width = 300,height=550)
+    pildiraam.config(bg="yellow")
+    pildiraam.pack(side="right")
 
-
-
-
-# Loome Tkinteri akna vajalike paramaatritega ja sinna sisse fraimi.
+    for i in range(9):
+        tk.Button(nupuraam,text=tähed[i],height=200,width = 70).grid(row=0,column=i)
+    tk.BUtton(nupuraam,text = "I").grid(row=1,column=1)
+    
 
 root = tk.Tk()
 root.title("Poomismäng")
@@ -61,13 +79,14 @@ Alumine_osa.pack(fill="y", expand=1, side="left")
 vasak_taust = tk.Label(Alumine_osa, image= peamine, width=1000, height= 550).pack(fill= "both", expand=1, pady = 0)
 
 
-nupp_alusta=tk.Button(Alumine_osa,text = "Alusta",font = ("arial",30), command=raskusastme_valimine)
+nupp_alusta=tk.Button(Alumine_osa,text = "Alusta",font = ("arial",30), command=põhiaken)
 nupp_alusta.place(x=425,y=92)
 nupp_alusta.config(image=muster, activebackground="green", anchor="center", bd=10, height=60, width= 120, compound="center")
 
 nupp2= tk.Button(Alumine_osa, text = "Välju",font = ("arial",24), command = root.destroy, compound="center")
 nupp2.place(x=435,y=200)
 nupp2.config(image=muster, activebackground="red", anchor="center", bd=10, height= 50, width= 100)
+
 
 
 root.mainloop()
---
+++ testproj.py
@@ -0,0 +1,34 @@
+def teesõnadjatähed(tekstifail):
+    f = open(tekstifail)
+    def num_there(s):
+        for i in s:
+            if i.isdigit():
+                return True
+        return False
+    
+    temp = []
+    for rida in f:
+        rida = rida.strip()
+        temp.append(rida)
+    f.close()
+    sõnad = []
+    for i in temp:
+        try:
+            if (not num_there(i) and not i[0].isupper()
+                and len(i) > 3
+                and "-" not in i and ","not in i
+                and "." not in i and "'" not in i
+                and "&" not in i and "\n" not in i
+                and "ž" not in i and " " not in i
+                and "é" not in i and "!" not in i
+                and "š" not in i and "x" not in i
+                and "y" not in i and "w" not in i):
+                sõnad.append(i)
+        except:
+            pass
+    
+    tähestik = set()
+    for sõna in sõnad:
+        for täht in sõna.strip():
+            tähestik.add(täht.lower())
+    return (sorted(sõnad),sorted(list(tähestik)))
