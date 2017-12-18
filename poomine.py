2aabcf2f2ed72a60610d088d39bc4d9fdb774e91
Leemet <33695127+markusleemet@users.noreply.github.com>
2017-12-18 16:53:14 +0200

    lisasin mängu alustus nupu

--- poomine.py
+++ poomine.py
@@ -3,9 +3,18 @@ from random import randint
 from testproj import teesõnadjatähed
 from tkinter import PhotoImage
 import winsound
-m = teesõnadjatähed("eesti_keel.txt")
-sõnad = m[0]
-tähed = m[1]
+
+
+
+sõnadjatähed = teesõnadjatähed("eesti_keel.txt")
+sõnad = sõnadjatähed[0]
+tähed = sõnadjatähed[1]
+arvamise_korrad = 0
+
+
+# Teen sõnade maatriksi, kus igal real on kindla pikkusega sõnad
+
+
 
 def ekraani_suurus(w, h):
     sw = root.winfo_screenwidth()
@@ -13,6 +22,53 @@ def ekraani_suurus(w, h):
     x = (sw - w) / 2
     y = (sh - h) / 2
     return ('%dx%d+%d+%d' % (w, h, x, y))
+
+
+def sõna_valimine(raskusaste):
+    if raskusaste == "kerge":
+        pikkus = randint(4, 6)
+    elif raskusaste == "keskmine":
+        pikkus = randint(7, 10)
+    else:
+        pikkus = randint(10, len(sõnad)-1)
+    sõna_asukoht = randint(0, len(sõnad[pikkus]) - 1)
+    sõna = sõnad[pikkus][sõna_asukoht]
+
+
+    vihje = ""
+    lisatavad_tähed = [sõna[0], sõna[-1]]
+    if raskusaste == "kerge":
+        for element in range(len(sõna)):
+            if element == 0 or element == len(sõna) - 1:
+                vihje = vihje + sõna[element]
+            elif sõna[element] in lisatavad_tähed:
+                vihje = vihje + sõna[element]
+            else:
+                vihje = vihje + "_"
+    elif raskusaste == "keskmine":
+        for element in range(len(sõna)):
+            if element == 0 or element == len(sõna) - 1:
+                lisatavad_tähed.append(sõna[element])
+                vihje = vihje + sõna[element]
+            else:
+                vihje = vihje + "_"
+    else:
+        vihje = "_" * len(sõna)
+
+    return vihje, sõna
+
+
+def sõna_uuendamine(pakkumine, sõna, eelmine_sõna):
+    uuendatud_sona = ""
+    for element in range(len(eelmine_sõna)):
+        if pakkumine == sõna[element]:
+            uuendatud_sona = uuendatud_sona + pakkumine
+        else:
+            uuendatud_sona = uuendatud_sona + eelmine_sõna[element]
+    return uuendatud_sona
+
+
+
 
 def raskusastme_valimine(valitud):
     global frame
@@ -44,32 +100,40 @@ def raskusastme_valimine(valitud):
     kerge = tk.Button(frame, text="Kerge")
     kerge.config(font=("arial", 24), width=100, height=50, image=muster, compound="center",
                        activebackground="green", bd=10, command=lambda: raskusastme_valimine("kerge"))
-    kerge.place(x=435, y=150)
+    kerge.place(x=435, y=230)
 
     keskmine = tk.Button(frame, text="Keskmine")
     keskmine.config(font=("arial", 24), width=150, height=50, image=muster, compound="center",
                  activebackground="orange", bd=10, command=lambda: raskusastme_valimine("keskmine"))
-    keskmine.place(x=410, y=240)
+    keskmine.place(x=410, y=320)
 
     raske = tk.Button(frame, text="Raske")
     raske.config(font=("arial", 24), width=100, height=50, image=muster, compound="center",
                  activebackground="red", bd=10, command=lambda: raskusastme_valimine("raske"))
-    raske.place(x=435, y=330)
+    raske.place(x=435, y=410)
+
+    mängima_nupp = tk.Button(frame, text="MÄNGIMA")
+    mängima_nupp.config(font=("arial", 30, "bold"), command=lambda: põhiaken(valitud), width=200, height=100, image=muster, compound="center",
+                        activebackground="red", bd=10)
+    if valitud != 1:
+        mängima_nupp.place(x=383, y=70)
     
     root.mainloop()
     
     
-def põhiaken():
-    #loon 3 eraldi frame, kuhu panna sõna, nupud ja poomispilt
+def põhiaken(raskus):
+    # loon 3 eraldi frame, kuhu panna sõna, nupud ja poomispilt
     global frame
     frame.pack_forget()
     taustraam = tk.Frame(root)
     taustraam.config(bg="green")
     taustraam.pack(fill="both")
+    taust =
 
     tekstiraam=tk.Frame(taustraam,width = 1000,height=200)
     tekstiraam.config(bg="red")
-    tekstiraam.pack() 
+    tekstiraam.pack(fill = "both", expand= 1)
+    tk.Label(tekstiraam,text = sõna_valimine(raskus)[0], font=("Arial", 40, "bold"), compound = "center").place(x = 50, y = 50)
     
     nupuraam = tk.Frame(taustraam,width = 700,height=500)
     nupuraam.pack(side="left")
@@ -88,8 +152,8 @@ def põhiaken():
         tk.Button(nupuraam,bg="green",width=100,font=("arial", 24),height=100,image = pixel
         ,text=str(tähed[i]),compound="c").place(x=10+(115*(i-18)),y=330)
     for i in range(24,27):
-        tk.Button(nupuraam,bg="green",width=200,font=("arial", 24),height=40,image = pixel
-        ,text=str(tähed[i]),compound="c").place(x=10+(235*(i-24)),y=440)
+        tk.Button(nupuraam,bg="green",width=200,font=("arial", 24),height=40,image = pixel,
+        text=str(tähed[i]),compound="c").place(x=10+(235*(i-24)),y=440)
         
     pildiraam = tk.Frame(taustraam,width = 300,height=500)
     pildiraam.config(bg="yellow")
@@ -99,6 +163,7 @@ def põhiaken():
 #funktsioon, kui nuppu vajutatakse
 def nupuvajutus(täht):
     winsound.PlaySound('pildid/nupp.wav', winsound.SND_FILENAME)
+    # sõna_uuendamine(arvamise_korrad, )
 
 def esiekraan():
     global frame
@@ -130,6 +195,7 @@ def esiekraan():
     nupp2 = tk.Button(Alumine_osa, text="Välju", font=("arial", 24), command=root.destroy, compound="center")
     nupp2.place(x=435, y=200)
     nupp2.config(image=muster, activebackground="red", anchor="center", bd=10, height=50, width=100)
+
     root.mainloop()
 # Loome Tkinteri akna vajalike paramaatritega ja sinna sisse fraimi.
 root = tk.Tk()
@@ -138,5 +204,4 @@ root.geometry(ekraani_suurus(1000, 700))
 root.resizable(width="false", height="false")
 frame = tk.Frame()
 frame.pack(fill=tk.BOTH, expand=1)
-print(len(tähed))
-põhiaken()
+esiekraan()
